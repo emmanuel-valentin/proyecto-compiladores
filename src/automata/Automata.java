3adc19003eea7311efe3babb50974e30bd4b37f5
Emmanuel Valentin <ema_110@outlook.es>
2023-03-11 12:14:16 -0600

    Add validations when you add a new transition

--- src/automata/Automata.java
+++ src/automata/Automata.java
@@ -29,14 +29,24 @@ public class Automata {
   }
 
   public void addTransition(Integer currentState, Integer nextState, String transition) {
+    // Si ya existe la transición, entoces no la añade al estado indicado.
     if (this.automata.get(currentState).containsKey(transition)) return;
+
+    // Si el estado actual no existe, entonces lo crea y le añade la transición.
+    if (this.automata.get(currentState) == null) {
+      Map<String, Integer> transitions = new HashMap<>();
+      this.automata.put(currentState, transitions);
+      return;
+    }
+    // Si el estado actual existe, entonces le añade la transición.
     this.automata.get(currentState).put(transition, nextState);
   }
 
   public boolean isValid(String word) {
     Integer currentState = this.initialState;
     for (char c : word.toCharArray()) {
-      // TODO: Iterate every character and move to the next state. Finally check if the last state is a final state.
+      // TODO: Itera cada caracter de la palabra a validar y verifica
+      //  si el último estado al que se mueve, es un estado final
       System.out.println(c);
     }
     return true;
