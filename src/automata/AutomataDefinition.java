025f7720d00b13ffd06823da6c19dc5a567de815
Emmanuel Valentin <ema_110@outlook.es>
2023-03-12 23:12:51 -0600

    Add automata example

---
+++ src/automata/AutomataDefinition.java
@@ -0,0 +1,39 @@
+package automata;
+
+import java.util.ArrayList;
+public class AutomataDefinition {
+  /**
+   * Este es ejemplo de un automata que valida cadenas que contengan un número par de 0,
+   * y sin símbolos 1 consecutivos.
+   */
+  public static Automata automata;
+  static {
+    ArrayList<Integer> finalStates = new ArrayList<>();
+    automata = new Automata();
+
+    automata.addTransition(0, 1, "0");
+    automata.addTransition(0, 3, "1");
+
+    automata.addTransition(1, 2, "0");
+    automata.addTransition(1, 5, "1");
+
+    automata.addTransition(2, 4, "0");
+    automata.addTransition(2, 3, "1");
+
+    automata.addTransition(3, 1, "0");
+    automata.addTransition(3, 6, "1");
+
+    automata.addTransition(4, 2, "0");
+    automata.addTransition(4, 5, "1");
+
+    automata.addTransition(5, 2, "0");
+    automata.addTransition(5, 6, "1");
+
+    automata.addTransition(6, 6, "0");
+    automata.addTransition(6, 6, "1");
+
+    finalStates.add(2);
+    finalStates.add(3);
+    automata.setFinalStates(finalStates);
+  }
+}
