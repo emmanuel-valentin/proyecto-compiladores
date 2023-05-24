69d2822aff8162756a2887f7abe5f97d7039410e
Emmanuel Valentin <ema_110@outlook.es>
2023-05-24 01:05:45 -0600

    Add Parser class

--- src/Parser.java
+++ src/Parser.java
@@ -1,2 +1,14 @@
-package PACKAGE_NAME;public class Parser {
+import java.util.List;
+
+public class Parser {
+  private final List<Token> tokens;
+  private Token lookahead;
+
+  public Parser(List<Token> tokens) {
+    this.tokens = tokens;
+  }
+
+  public void parse() {
+    // TODO: Implementar analizador sintáctico.
+  }
 }
